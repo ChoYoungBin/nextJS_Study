43b5600b96920ca81d92c8259f981db8b5efae12
JoYeongBin <whdudqls_1@naver.com>
2023-07-12 00:18:10 +0900

    #Day2 added Link, Meetup component with css

--- React-Review/src/App.js
+++ React-Review/src/App.js
@@ -2,10 +2,12 @@ import { Route, Routes } from 'react-router-dom'
 import AllMeetups from './pages/AllMeetups'
 import Favorites from './pages/Favorites'
 import NewMeetups from './pages/NewMeetups'
+import MainNavigation from './components/layout/MainNavigation'
 
 function App() {
 	return (
 		<>
+			<MainNavigation />
 			<Routes>
 				<Route path='/' element={<AllMeetups />} />
 				<Route path='/new-meetup' element={<NewMeetups />} />
