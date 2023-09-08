a2e5e927b2ab52deaa1caf78d861a85693cca76d
joyeongbin <3170024@kbfg.com>
2023-09-08 12:14:35 +0900

    added _document.js

--- nextjs-course/pages/_app.js
+++ nextjs-course/pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Next Events</title>
+        <meta
+          name="viewport"
+          contetn="initial-scale=1.0, width=device-width"
+        ></meta>
+        <meta name="description" context="NextJs"></meta>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- nextjs-course/pages/blog/[...slug].js
+++ nextjs-course/pages/blog/[...slug].js
@@ -2,16 +2,20 @@ import { useRouter } from "next/router";
 import Head from "next/head";
 
 const BlogPostsPage = (props) => {
+  const pageHeadData = (
+    <Head>
+      <title>Filtered Blogs</title>
+      <meta name="description" content={`${props.title} `}></meta>
+    </Head>
+  );
+
   const router = useRouter();
 
   console.log(router.query);
 
   return (
     <>
-      <Head>
-        <title>Filtered Blogs</title>
-        <meta name="description" content={`${props.title} `}></meta>
-      </Head>
+      {pageHeadData}
       <div>
         <h1>The Blog Posts</h1>
       </div>
