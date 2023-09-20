3a423f5b86b04b4b080c569bb5bbd8ad539e3688
JoYeongBin <whdudqls_1@naver.com>
2023-09-20 23:35:13 +0900

    #Day15 added feedback API

--- nextjs-api-course/pages/index.js
+++ nextjs-api-course/pages/index.js
@@ -1,38 +1,51 @@
-import { useRef } from "react";
+import { useRef } from 'react'
 
 const HomePage = () => {
-  const emailInput = useRef();
-  const feedbackInput = useRef();
+	const emailInput = useRef()
+	const feedbackInput = useRef()
 
-  const submitFormHandler = (event) => {
-    event.preventDefault();
+	const submitFormHandler = (event) => {
+		event.preventDefault()
 
-    const enteredEmail = emailInput.current.value;
-    const enteredFeebback = feedbackInput.current.value;
+		const enteredEmail = emailInput.current.value
+		const enteredFeebback = feedbackInput.current.value
 
-    fetch(); //{email : 'test@mail.com', text:'some feedback'}
-    //handler();
-  };
+		const requestBody = { email: enteredEmail, text: enteredFeebback }
 
-  return (
-    <>
-      <div>
-        <h1>The Home Page</h1>
-        <form onSubmit={submitFormHandler}>
-          <div>
-            <label htmlFor="email">Your Email Address</label>
-            <input type="email" id="email" ref={emailInput} />
-          </div>
+		fetch('/api/feedback', {
+			method: 'POST',
+			body: JSON.stringify(requestBody),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then((response) => {
+				response.json()
+			})
+			.then((data) => console.log(data))
+		//{email : 'test@mail.com', text:'some feedback'}
+		//handler();
+	}
 
-          <div>
-            <label htmlFor="feedback">Your FeedBack</label>
-            <textarea id="feedback" rows="5" ref={feedbackInput} />
-          </div>
-          <button>Send Button </button>
-        </form>
-      </div>
-    </>
-  );
-};
+	return (
+		<>
+			<div>
+				<h1>The Home Page</h1>
+				<form onSubmit={submitFormHandler}>
+					<div>
+						<label htmlFor='email'>Your Email Address</label>
+						<input type='email' id='email' ref={emailInput} />
+					</div>
 
-export default HomePage;
+					<div>
+						<label htmlFor='feedback'>Your FeedBack</label>
+						<textarea id='feedback' rows='5' ref={feedbackInput} />
+					</div>
+					<button>Send Button </button>
+				</form>
+			</div>
+		</>
+	)
+}
+
+export default HomePage
