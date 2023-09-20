2b6502f3f36a55e556c0c2c2fe35fcba06c60287
JoYeongBin <whdudqls_1@naver.com>
2023-09-21 00:19:33 +0900

    #Day15 fixed feedback folder for dynamic API routing

--- nextjs-api-course/pages/feedback/index.js
+++ nextjs-api-course/pages/feedback/index.js
@@ -5,7 +5,7 @@ const FeedbackPage = (props) => {
 	const [feedbackData, setFeedbackData] = useState()
 
 	const loadFeedbackHandler = (id) => {
-		fetch(`/api/${id}`)
+		fetch(`/api/feedback/${id}`)
 			.then((response) => response.json())
 			.then((data) => setFeedbackData(data.feedback))
 	}
